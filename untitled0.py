e82e9fe5769382025ff62c8e47e59ac3e9b33339
yacinelol24 <yacine.benkirane@gmail.com>
2021-01-13 14:57:43 -0500

    Co-57
    Added Graphs for Co

---
+++ untitled0.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 15:21:40 2021
+
+@author: Yacine Benkirane
+"""
+
+import spinmob as s
+import mcphysics
+
+
+#mcphysics.playground.fitting_statistics_demo()
+
+#mcphysics.data.load_chn()
+
+mcphysics.data.plot_chns()
