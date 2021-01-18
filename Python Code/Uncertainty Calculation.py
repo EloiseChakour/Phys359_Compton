1f60d2b621f5ce101a2ad70650f015bdfae49385
YukuanZhao <77301652+YukuanZhao@users.noreply.github.com>
2021-01-18 16:28:48 -0500

    Update Uncertainty Calculation.py

--- Python Code/Uncertainty Calculation.py
+++ Python Code/Uncertainty Calculation.py
@@ -21,6 +21,7 @@ summed_databox = mcp.data.load_chns(combine=True)
 x = summed_databox[0]
 y = summed_databox[1]
 
+#Enter the range
 a=1200
 b=1550
 
@@ -33,10 +34,14 @@ avg = sum(shortened_y )/len(shortened_y )
 squares=[]
 for n in shortened_y:
     squares.append(n**2)
-print(squares)
 
+    
+    #print(squares)
+
+#Variance
 var=abs((sum(squares)/len(squares))-avg**2)
 
+#Standard Deviation
 std=(var)**(1/2)
 
 error=std/(len(shortened_y))**(0.5)
