745fa4429e12da59a25250a76011099e6ece7ac0
YukuanZhao <77301652+YukuanZhao@users.noreply.github.com>
2021-01-18 17:41:11 -0500

    Update Uncertainty Calculation.py

--- Python Code/Uncertainty Calculation.py
+++ Python Code/Uncertainty Calculation.py
@@ -42,3 +42,25 @@ error=std/(10*len(shortened_y))**(0.5)
 
 
 print(error)
+
+##################################################
+
+import mcphysics as mcp
+import numpy as np
+#Loading and reformatting data
+#summed_databox = mcp.data.load_chns(combine=True)
+summed_databox = mcp.data.load_chns(combine=True)
+x = summed_databox[0]
+y = summed_databox[1]
+
+a=75
+b=120
+
+shortened_x = x[a:b]
+shortened_y = y[a:b]
+
+A=np.cov([shortened_y])
+
+print(A**0.5)
+
+########################################
